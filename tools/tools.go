fae734c8e8463c808f7688bc38b10b922413051d
Ethan Li <lietk12@gmail.com>
2023-10-05 19:28:02 -0700

    Make `/tools` work with go1.21, add spellchecker

--- tools/tools.go
+++ tools/tools.go
@@ -8,6 +8,7 @@ package tools
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 // https://github.com/golang/go/issues/25922
 import (
+	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
 	_ "mvdan.cc/gofumpt"
