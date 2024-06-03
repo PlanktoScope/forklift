9bbe9b2ac471fb6a12934683dbf9cec279d75116
Ethan Li <lietk12@gmail.com>
2024-06-02 21:29:02 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 mkdir -p zig-build
-ZIG_GLOBAL_CACHE_DIR="$(pwd)/zig-build"
-ZIG_LOCAL_CACHE_DIR="$(pwd)/zig-build"
+export ZIG_GLOBAL_CACHE_DIR="$(pwd)/zig-build"
+export ZIG_LOCAL_CACHE_DIR="$(pwd)/zig-build"
+zig env 1>&2
 zig cc $@ 1>&2
