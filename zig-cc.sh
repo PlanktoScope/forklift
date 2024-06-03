2235d5cb2b30b8808a390c5c2851ed50da3e914c
Ethan Li <lietk12@gmail.com>
2024-06-02 17:42:01 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -2,8 +2,8 @@
 
 log="$(mktemp)"
 
-echo $@
+echo "zig-cc args:" $@ | tee $log
 
 exit_code=0
-zig cc $@ 2>&1 || exit_code=$? | tee $log
+zig cc $@ 2>&1 || exit_code=$? | tee --append $log
 exit $exit_code
