e088b59a57a575cd7c7599b8950bb1f071a12d58
Ethan Li <lietk12@gmail.com>
2024-06-02 21:21:56 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-ls -l /tmp 1>&2
-
-cat /tmp/cgo-gcc-input-*.c 1>&2 || true
-zig cc --verbose $@ 1>&2
+mkdir -p zig-build
+ZIG_GLOBAL_CACHE_DIR="$(pwd)/zig-build"
+ZIG_LOCAL_CACHE_DIR="$(pwd)/zig-build"
+zig cc $@ 1>&2
