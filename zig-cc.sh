9577b6e4609526bab06c90db97e1c8f0e07b9073
Ethan Li <lietk12@gmail.com>
2024-06-03 09:42:51 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -3,11 +3,17 @@
 sudo chown $USER -R $(pwd)
 sudo chown $USER -R $ZIG_GLOBAL_CACHE_DIR
 sudo chown $USER -R $ZIG_LOCAL_CACHE_DIR
-ls -l $ZIG_LOCAL_CACHE_DIR/.. 1>&2
-ls -l $ZIG_LOCAL_CACHE_DIR 1>&2
-ls -l $(pwd)/.. 1>&2
-ls -l $(pwd) 1>&2
-zig env 1>&2
-echo "zig cc $@" 1>&2
-strace zig cc $@ 1>&2
+if ls -l $(pwd) | grep "fsevents"; then
+  ls -l $(pwd)/.. 1>&2
+  ls -l $(pwd) 1>&2
+  ls -l /tmp 1>&2
+  sudo chown $USER /tmp/cgo-gcc-input-*.o
+  sudo chmod a+w /tmp/cgo-gcc-input-*.o
+  ls -l /tmp 1>&2
+  echo "zig cc $@" 1>&2
+  strace zig cc $@ 1>&2
+  exit $?
+fi
+
+zig cc $@ 1>&2
 exit $?
