c59fe44fdf6171d339dee262cabda229518ba71e
Ethan Li <lietk12@gmail.com>
2024-06-02 20:13:50 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -1,16 +1,10 @@
 #!/bin/bash -x
 
-if ls | grep "fsevents" > /dev/null; then
-  pwd 1>&2
-  ls -l 1>&2
-  ls -l /tmp 1>&2
-fi
+ls -l /tmp 1>&2
 
 zig cc $@ 1>&2
 exit_code=$?
 
-if ls | grep "fsevents" > /dev/null; then
-  ls -l /tmp 1>&2
-fi
+ls -l /tmp 1>&2
 
 exit $exit_code
