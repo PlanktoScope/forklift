aa9793321ae09bf506b748365503c01c77a0bde8
Ethan Li <lietk12@gmail.com>
2024-06-02 18:31:50 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-echo "zig-cc args:" $@
+echo "workspace: $WORK"
+ls -lR $WORK
+
+echo "zig cache: $ZIG_GLOBAL_CACHE_DIR"
+ls -l $ZIG_GLOBAL_CACHE_DIR
+
+echo "zig-cc args: $@"
 
 zig cc $@
 
-echo "workspace:" $WORK
+echo "zig cache: $ZIG_GLOBAL_CACHE_DIR"
+ls -l $ZIG_GLOBAL_CACHE_DIR
+
+echo "workspace parent:"
 ls -l $WORK/..
+echo "workspace: $WORK"
 ls -lR $WORK
