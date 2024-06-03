332a47200cf678f201afa38da79cc44ddc52bf78
Ethan Li <lietk12@gmail.com>
2024-06-02 19:53:54 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -3,16 +3,14 @@
 if ls | grep "fsevents" > /dev/null; then
   pwd 1>&2
   ls -l 1>&2
-  cat /tmp/cgo-gcc-input-*.c 1>&2
-  ls /home/runner/go/pkg/mod/github.com/fsnotify/fsevents@v0.2.0/
+  ls -l /tmp
 fi
 
 zig cc $@
 exit_code=$?
 
 if ls | grep "fsevents" > /dev/null; then
-  zig env 1>&2
-  ls -l 1>&2
+  ls -l /tmp 1>&2
 fi
 
 exit $exit_code
