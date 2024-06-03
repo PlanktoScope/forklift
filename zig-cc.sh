f3f68aa485fc252d02be2589ab5a12ca2fe17f1f
Ethan Li <lietk12@gmail.com>
2024-06-02 19:31:31 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -x
 
-if ls "$WORK" | grep "fsevents" > /dev/null; then
+if ls | grep "fsevents" > /dev/null; then
   pwd 1>&2
-  ls -l $WORK 1>&2
+  ls -l 1>&2
   cat /tmp/cgo-gcc-input-* 1>&2
   ls /home/runner/go/pkg/mod/github.com/fsnotify/fsevents@v0.2.0/
 fi
 
-if ls "$WORK" | grep "fsevents" > /dev/null; then
+zig cc $@
+
+if ls | grep "fsevents" > /dev/null; then
   zig env 1>&2
-  zig cc $@
-  ls -l $WORK 1>&2
+  ls -l 1>&2
 fi
