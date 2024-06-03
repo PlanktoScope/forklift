2caaeb2b26fcf2d9bf0bf9e1a7c52ef5843a20e0
Ethan Li <lietk12@gmail.com>
2024-06-02 18:54:08 -0700

    Continu troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
-echo "workspace: $WORK" 1>&2
-ls -lR $WORK 1>&2
-
-echo "zig cache: $ZIG_GLOBAL_CACHE_DIR" 1>&2
-ls -l $ZIG_GLOBAL_CACHE_DIR 1>&2
-
 echo "zig-cc args: $@" 1>&2
+go env 1>&2
 
 zig cc $@
-
-echo "zig cache: $ZIG_GLOBAL_CACHE_DIR" 1>&2
-ls -l $ZIG_GLOBAL_CACHE_DIR 1>&2
-
-echo "workspace parent:" 1>&2
-ls -l $WORK/.. 1>&2
-echo "workspace: $WORK" 1>&2
-ls -lR $WORK 1>&2
