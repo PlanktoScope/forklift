0574ff6ab775d46588aaf6213cf58ac96b414bd3
Ethan Li <lietk12@gmail.com>
2024-06-03 08:55:33 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -1,6 +1,8 @@
-#!/bin/sh -e
+#!/bin/bash -eux
 
 sudo chown $USER -R $(pwd)
 sudo chown $USER -R $ZIG_GLOBAL_CACHE_DIR
 sudo chown $USER -R $ZIG_LOCAL_CACHE_DIR
+echo "zig cc $@" 1>&2
 zig cc $@ 1>&2
+exit $?
