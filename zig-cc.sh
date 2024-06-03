d4ed9e58432f9cd8f12845d0f869ebdf8aa66153
Ethan Li <lietk12@gmail.com>
2024-06-03 09:03:23 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -3,6 +3,11 @@
 sudo chown $USER -R $(pwd)
 sudo chown $USER -R $ZIG_GLOBAL_CACHE_DIR
 sudo chown $USER -R $ZIG_LOCAL_CACHE_DIR
+ls -l $ZIG_LOCAL_CACHE_DIR/.. 1>&2
+ls -l $ZIG_LOCAL_CACHE_DIR 1>&2
+ls -l $(pwd)/.. 1>&2
+ls -l $(pwd) 1>&2
+zig env 1>&2
 echo "zig cc $@" 1>&2
 zig cc $@ 1>&2
 exit $?
