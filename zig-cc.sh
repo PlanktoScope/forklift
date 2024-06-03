517912dd578ece384d264bc5ddfaa4553b3af606
Ethan Li <lietk12@gmail.com>
2024-06-03 10:33:15 -0700

    Continue troubleshooting...

--- zig-cc.sh
+++ zig-cc.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -eux
 
 chmod +w $(pwd)
-if ls -l $(pwd) | grep "fsevents"; then
-  ls -l $(pwd)/.. 1>&2
-  ls -l $(pwd) 1>&2
-  echo "zig cc $@" 1>&2
-fi
-
 zig cc $@ 1>&2
-exit $?
+exit_code=$?
+ls -l $(pwd)
+exit $exit_code
