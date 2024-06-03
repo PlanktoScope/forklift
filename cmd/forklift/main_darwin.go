afd18b940b47330b8d0af3caead181befaacb812
Ethan Li <lietk12@gmail.com>
2024-06-03 15:15:32 -0700

    Remove CGo preamble

--- cmd/forklift/main_darwin.go
+++ cmd/forklift/main_darwin.go
@@ -1,8 +1,9 @@
 //go:build darwin
 
-package main
-
 // This is needed because Forklift imports Docker Compose, which on darwin imports
 // github.com/fsnotify/fsevents, which requires CGo; and we need to make CGo do something so that
 // we can generate CGo runtime stuff for the linker:
+
+package main
+
 import "C"
